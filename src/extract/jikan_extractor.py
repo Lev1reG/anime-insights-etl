2f72933ab73802589a16e7bd2bab20cfc1886449
Lev1reG <derentanaphan@mail.ugm.ac.id>
2024-11-19 13:35:30 +0700

    feat: automate extract data for all page

--- src/extract/jikan_extractor.py
+++ src/extract/jikan_extractor.py
@@ -15,95 +15,120 @@ class JikanExtractor:
     def __init__(self):
         self.base_url = BASE_URL
 
-    def fetch_anime_by_season(self, year, season, page=1):
+    def fetch_anime_by_season(self, year, season):
         """
         Fetch anime by year and season from Jikan API.
 
         Args:
             year (int): The year of the anime season.
             season (str): The season (e.g., 'winter', 'spring', 'summer', 'fall').
-            page (int): The page number of the results.
 
         Returns:
-            dict: JSON response containing anime data.
+            list: List of dictionaries containing anime data.
         """
         endpoint = f"{self.base_url}/seasons/{year}/{season}"
-        params = {"page": page}
+        records = []
+        page = 1
 
-        try:
-            response = requests.get(endpoint, params=params)
-            response.raise_for_status()
-            logging.info(
-                f"Successfully fetched anime data for {season} {year}, page {page}."
-            )
-            data = response.json()
-            anime_list = data["data"]
-            records = []
-            for anime in anime_list:
-                records.append(
-                    {
-                        "mal_id": anime.get("mal_id"),
-                        "title": anime.get("title"),
-                        "status": anime.get("status"),
-                        "type": anime.get("type"),
-                        "episodes": anime.get("episodes"),
-                        "score": anime.get("score"),
-                        "start_date": anime.get("aired", {}).get("from"),
-                        "end_date": anime.get("aired", {}).get("to"),
-                        "popularity": anime.get("popularity"),
-                        "favorites": anime.get("favorites"),
-                        "genres": [genre["name"] for genre in anime.get("genres", [])],
-                        "rating": anime.get("rating"),
-                        "producers": [
-                            producer["name"] for producer in anime.get("producers", [])
-                        ],
-                        "studios": [
-                            studio["name"] for studio in anime.get("studios", [])
-                        ],
-                    }
+        while True:
+            params = {"page": page}
+
+            try:
+                response = requests.get(endpoint, params=params)
+                response.raise_for_status()
+                logging.info(
+                    f"Successfully fetched anime data for {season} {year}, page {page}."
                 )
+                data = response.json()
+                anime_list = data["data"]
 
-            return records
-        except requests.exceptions.RequestException as e:
-            logging.error(f"Error while fetching anime data: {e}")
-            return None
+                if not anime_list:
+                    logging.info(
+                        f"No more data available after page {page}. All pages have been fetched."
+                    )
+                    break
 
-    def fetch_anime_reviews(self, mal_id, page=1):
+                for anime in anime_list:
+                    records.append(
+                        {
+                            "mal_id": anime.get("mal_id"),
+                            "title": anime.get("title"),
+                            "type": anime.get("type"),
+                            "episodes": anime.get("episodes"),
+                            "score": anime.get("score"),
+                            "start_date": anime.get("aired", {}).get("from"),
+                            "end_date": anime.get("aired", {}).get("to"),
+                            "popularity": anime.get("popularity"),
+                            "genres": [
+                                genre["name"] for genre in anime.get("genres", [])
+                            ],
+                            "producers": [
+                                producer["name"]
+                                for producer in anime.get("producers", [])
+                            ],
+                            "studios": [
+                                studio["name"] for studio in anime.get("studios", [])
+                            ],
+                        }
+                    )
+                page += 1
+            except requests.exceptions.RequestException as e:
+                logging.error(f"Error while fetching anime data: {e}")
+                break
+
+        return records
+
+    def fetch_anime_reviews(self, mal_id):
         """
-        Fetch reviews for a specific anime from Jikan API.
-        
+        Fetch all reviews for a specific anime from Jikan API.
+
         Args:
             mal_id (int): The MyAnimeList ID of the anime.
-            page (int): The page number of the reviews to fetch.
 
         Returns:
             list: List of dictionaries containing anime reviews.
         """
         endpoint = f"{self.base_url}/anime/{mal_id}/reviews"
-        params = {"page": page}
+        records = []
+        page = 1
 
-        try:
-            response = requests.get(endpoint, params=params)
-            response.raise_for_status()
-            logging.info(f"Successfully fetched reviews for anime ID {mal_id}, page {page}.")
-            data = response.json()
-            review_list = data["data"]
-            records = []
-            for review in review_list:
-                records.append(
-                    {
-                        "mal_id": mal_id,
-                        "username": review.get("user", {}).get("username"),
-                        "tags": review.get("tags", []),
-                        "episodes_watched": review.get("episodes_watched"),
-                        "review": review.get("review"),
-                        "score": review.get("score"),
-                    }
+        while True:
+            params = {"page": page}
+
+            try:
+                response = requests.get(endpoint, params=params)
+                response.raise_for_status()
+                logging.info(
+                    f"Successfully fetched reviews for anime ID {mal_id}, page {page}."
                 )
-            return records
-        except requests.exceptions.RequestException as e:
-            logging.error(f"Error while fetching reviews for anime ID {mal_id}: {e}")
-            return None
+                data = response.json()
+                review_list = data["data"]
+
+                if not review_list:
+                    logging.info(
+                        f"No more reviews available after page {page}. All pages have been fetched."
+                    )
+                    break
+
+                for review in review_list:
+                    records.append(
+                        {
+                            "mal_id": mal_id,
+                            "username": review.get("user", {}).get("username"),
+                            "tags": review.get("tags", []),
+                            "episodes_watched": review.get("episodes_watched"),
+                            "review": review.get("review"),
+                            "score": review.get("score"),
+                        }
+                    )
+                page += 1
+            except requests.exceptions.RequestException as e:
+                logging.error(
+                    f"Error while fetching reviews for anime ID {mal_id}, page {page}: {e}"
+                )
+                break
+
+        return records
 
     def extract_to_dataframe(self, data):
         """
@@ -135,7 +160,7 @@ if __name__ == "__main__":
     # Fetch anime by season
     year = 2021
     season = "summer"
-    anime_data = extractor.fetch_anime_by_season(year, season, page=1)
+    anime_data = extractor.fetch_anime_by_season(year, season)
 
     if anime_data:
         anime_df = extractor.extract_to_dataframe(anime_data)
@@ -143,8 +168,8 @@ if __name__ == "__main__":
 
         # Fetch and print reviews for the first anime in the DataFrame
         if not anime_df.empty:
-            mal_id = anime_df.iloc[0]['mal_id']
-            reviews_data = extractor.fetch_anime_reviews(mal_id, page=1)
+            mal_id = anime_df.iloc[0]["mal_id"]
+            reviews_data = extractor.fetch_anime_reviews(mal_id)
             if reviews_data:
                 reviews_df = extractor.extract_to_dataframe(reviews_data)
                 print(reviews_df.head())
